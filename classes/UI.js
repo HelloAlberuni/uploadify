196f18677784dc524b6afe6a56723c9b2bc3f7d1
Alberuni Azad <azad.bbc@gmail.com>
2023-11-21 06:28:53 +0600

    Added all the basic features

---
+++ classes/UI.js
@@ -0,0 +1,145 @@
+class UI{
+    constructor(){
+        this.uploadify      = document.getElementById('uploadify');
+        this.dropArea       = document.getElementById('drop-zone');
+        this.uploadButton   = document.querySelector('button[type="submit"]');
+        this.preview        = document.querySelector('.preview-image');
+        this.copyLink       = document.querySelector('.copy-link');
+        this.copyLinkButton = document.querySelector('.button-copy-link');
+        this.uploadAnother  = document.querySelector('.button-upload-another');
+        this.tooltipText    = document.querySelector('.tooltiptext');
+        this.browseFile     = document.querySelector('.browse-files');
+        this.fileInput      = document.querySelector('.file-input');
+
+        this.whatsapp       = document.querySelector('.whatsapp a');
+        this.facebook       = document.querySelector('.facebook a');
+        this.twitter        = document.querySelector('.twitter a');
+        this.email          = document.querySelector('.email a');
+
+        this.notification          = document.querySelector('.notification');
+        this.notificationIcon      = document.querySelector('.notification-icon ');
+        this.notificationMessage   = document.querySelector('.notification-message');
+
+        // Set state
+        this.setState( '' );
+    }
+
+    updateProgress(percent){
+        let $progressTrack = document.querySelector('.progress-track');
+        let $progressText = document.querySelector('.progress-bar > span');
+            
+        $progressTrack.style.width = percent + '%';
+        $progressText.textContent = percent + '%';
+    }
+
+    displayImage( file ){
+        const _this = this;
+        let reader  = new FileReader();
+        
+        reader.readAsDataURL(file);
+        reader.onloadend = function(){
+            _this.setState('preview');
+
+            let img             = document.createElement('img');
+                img.src         = reader.result;
+                img.style.width = '100%'; // For svg file
+    
+            // Alread has an image, replace that with new
+            if( _this.preview.firstElementChild ){
+                _this.preview.firstElementChild.src = reader.result;
+            } else {
+                _this.preview.appendChild(img);
+            }
+            
+        }
+
+        uiInstance.uploadButton.disabled = false;
+    }
+
+    updateShareIconLinks( imageUrl ){
+        let whatsappUrl = `whatsapp://send?text=Check out this image: ${imageUrl}L`;
+        let facebookUrl = `https://www.facebook.com/sharer/sharer.php?u=${imageUrl}`;
+        let twitterUrl = `https://twitter.com/intent/tweet?url=${imageUrl}&text=Check out this image:`;
+        let emailUrl = `mailto:?subject=Check%20out%20this%20image&body=Hey,%20I%20found%20this%20cool%20image:%20${imageUrl}`;
+    
+        this.whatsapp.href = whatsappUrl;
+        this.facebook.href = facebookUrl;
+        this.twitter.href  = twitterUrl;
+        this.email.href    = emailUrl;
+    }
+
+    setState( state = '' ){
+        switch ( state ) {
+            case 'default':
+                this.defaultState();
+                break;
+
+            case 'preview':
+                this.previewState();
+                break;
+
+            case 'uploading':
+                this.uploadingSate();
+                break;
+
+            case 'uploaded':
+                this.uploadedState();
+                break;
+        
+            default:
+                break;
+        }
+    }
+
+    defaultState(){        
+        this.innerHTML             = '';
+        this.uploadify.classList   = 'uploadify';
+    }
+
+    previewState(){        
+        this.uploadify.classList.add('state--preview');
+    }
+
+    uploadingSate(){        
+        this.uploadify.classList.remove('state--preview');
+        this.uploadify.classList.add('state--uploading');
+        this.uploadButton.disabled = true;
+    }
+
+    uploadedState(){        
+        this.uploadify.classList.remove('state--uploading');
+        this.uploadify.classList.add('state--uploaded');
+    }
+
+    displayCopyUrl( url ){
+        this.copyLink.children[0].value = url;
+    }
+
+    displayNotice( message = '', status = '' ){
+        const _this = this;
+
+        let icon;
+        if( status === 'alert' ){
+            icon = '<svg viewBox="64 64 896 896" focusable="false" data-icon="exclamation-circle" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm-32 232c0-4.4 3.6-8 8-8h48c4.4 0 8 3.6 8 8v272c0 4.4-3.6 8-8 8h-48c-4.4 0-8-3.6-8-8V296zm32 440a48.01 48.01 0 010-96 48.01 48.01 0 010 96z"></path></svg>';
+        } else {
+            icon = '';
+        }
+
+        this.setState('default');       
+        this.notification.classList.add(status);
+        this.notification.classList.remove('d--none', 'animation-fade-out');
+        this.notification.classList.add('animation-slide-in');
+
+        this.notificationIcon.innerHTML      = icon;
+        this.notificationMessage.textContent = message;
+
+        console.log(message);
+    }
+
+    clearNotice(){   
+        if( this.notification.classList.contains('animation-slide-in') ){
+            this.notification.classList.remove('animation-slide-in');
+            this.notification.classList.add('animation-fade-out');
+        }
+    }
+}
---
+++ classes/Uploader.js
@@ -0,0 +1,63 @@
+class Uploader{
+    constructor(){
+        this.apiEndpoint = 'https://api.imgbb.com/1/upload';
+        this.apiKey      = '69e20fc50e2631b5b327fef0c291bb52';
+        this.expiration  = 600;
+    }
+
+    uploadFile( file ) {        
+        uiInstance.setState('uploading');
+      
+        const formData = new FormData();
+        formData.append('expiration', this.expiration);
+        formData.append('key', this.apiKey);
+        formData.append('image', file);
+      
+        let xhr = new XMLHttpRequest();
+        xhr.open('POST', this.apiEndpoint, true);
+      
+        xhr.onreadystatechange = function () {
+          if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+
+              // Request was successful
+              const response = JSON.parse(xhr.responseText);
+              const imageUrl = response.data.display_url;
+
+              uiInstance.displayCopyUrl(imageUrl);
+              uiInstance.updateProgress(100);
+    
+              // Intentionally create a little delay to wait for showing 100%.(it take a few moment based on the network speed, while downloading the image after upload)
+              setTimeout(function(){
+                uiInstance.setState('uploaded');
+                uiInstance.updateShareIconLinks( imageUrl );
+              }, 300 );
+
+            } else {
+
+              // Handle errors
+              if (xhr.status === 404) {
+                uiInstance.displayNotice( 'Resource not found:' + xhr.responseText, 'alert');
+              } else if (xhr.status === 401) {
+                uiInstance.displayNotice( 'Unauthorized request:' + xhr.responseText, 'alert');
+              } else {
+                uiInstance.displayNotice( 'Server error:' + xhr.responseText, 'alert');
+              }
+
+            }
+          }
+        };
+    
+        xhr.upload.onprogress = function(e){
+            if ( e.lengthComputable ) {
+                let percent = Math.round( (e.loaded / e.total) * 100 );
+    
+                if( percent < 100 ){
+                    uiInstance.updateProgress( percent );
+                }
+            }
+        }
+      
+        xhr.send(formData);
+    }
+}
